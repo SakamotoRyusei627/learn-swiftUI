49f006690c40a1b2267b34de2101a3067cb9d972
SakamotoRyusei627 <atksr0510@icloud.com>
2025-08-31 22:56:48 +0900

    Initial Commit

---
+++ learn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learn
+//
+//  Created by 坂本龍征 on 2025/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by 坂本龍征 on 2025/08/31.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
