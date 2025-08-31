85f628af7bfa78e91246d1ef5b3052bedfa0853a
SakamotoRyusei627 <atksr0510@icloud.com>
2025-09-01 08:35:21 +0900

    first commit

--- learn/ContentView.swift
+++ learn/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var todos = ["牛乳を買う","コードを書く","散歩する"]
+    @State private var newTodo = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("新しいTodoを入力する", text: $newTodo)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    Button("追加"){
+                        if !newTodo.isEmpty{
+                            todos.append(newTodo)
+                            newTodo = ""
+                        }
+                    }
+                    .padding(.trailing)
+                }
+                List {
+                    ForEach(todos,id:\.self){ todo in
+                        NavigationLink(value: todo){
+                            Text(todo)
+                        }
+                    }
+                }
+                .navigationDestination(for: String.self){todo in TodoDetailView(todo: todo)
+                }
+                .navigationTitle("Todoリスト")
+            }
+        }
+    }
+}
+
+struct TodoDetailView: View {
+    let todo: String
+    
+    var body: some View {
+        VStack(spacing: 20){
+            Text("Todoの詳細")
+                .font(.headline)
+            Text(todo)
+                .font(.largeTitle)
         }
         .padding()
     }
